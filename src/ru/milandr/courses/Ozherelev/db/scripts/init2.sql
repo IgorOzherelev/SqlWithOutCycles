e3f58357483ba7d5bb23a11115244156d1890044
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2018-11-12 21:47:51 +0300

    Update init2.sql

--- src/ru/milandr/courses/Ozherelev/db/scripts/init2.sql
+++ src/ru/milandr/courses/Ozherelev/db/scripts/init2.sql
@@ -8,29 +8,29 @@ create table if not exists players (
 
 /*script for true result (just a copy of init1.sql)*/
 /*
-INSERT INTO "players" (id, nickname, country) VALUES (1, 'Violent Joker', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (2, 'Gipermaxi4', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (3, 'Inspirarion', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (4, 'Korben Dalos', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (5, 'Pushkin', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (6, 'Emil Zolya', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (7, 'Snek', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (8, 'shiSava', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (9, 'Magicpie', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (10, 'King_Kong', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (11, 'The nineth wall', 'Russia Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (1, 'Violent Joker', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (2, 'Gipermaxi4', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (3, 'Inspirarion', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (4, 'Korben Dalos', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (5, 'Pushkin', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (6, 'Emil Zolya', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (7, 'Snek', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (8, 'shiSava', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (9, 'Magicpie', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (10, 'King_Kong', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (11, 'The nineth wall', 'Russian Federation');
 */
 
 /*script for false result */
 
-INSERT INTO "players" (id, nickname, country) VALUES (1, 'Hitman', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (2, 'GiperIgri4', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (3, 'Inspirarion', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (4, 'Korben Dalos', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (5, 'Evgeniy Onegin', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (6, 'Emil Zolya', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (7, 'Shadow Witch', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (8, 'Konek', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (9, 'Small Rat', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (10, 'King_Kong', 'Russia Federation');
-INSERT INTO "players" (id, nickname, country) VALUES (11, 'The ninth wall', 'Russia Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (1, 'Hitman', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (2, 'GiperIgri4', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (3, 'Inspirarion', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (4, 'Korben Dalos', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (5, 'Evgeniy Onegin', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (6, 'Emil Zolya', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (7, 'Shadow Witch', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (8, 'Konek', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (9, 'Small Rat', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (10, 'King_Kong', 'Russian Federation');
+INSERT INTO "players" (id, nickname, country) VALUES (11, 'The ninth wall', 'Russian Federation');
